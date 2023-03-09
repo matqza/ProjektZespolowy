aa5a0349ce6ea84d35db151ba38b7bae497d2e7b
Mateusz Kuźmoa <matix666@outlook.pl>
2023-03-09 19:16:35 +0100

    szkielet html

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nazwa strony</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <!-- zawartość nagłówka strony -->
+    </header>
+
+    <nav>
+        <!-- zawartość menu nawigacyjnego strony -->
+    </nav>
+
+    <main>
+        <!-- zawartość główna strony -->
+    </main>
+
+    <footer>
+        <!-- zawartość stopki strony -->
+    </footer>
+</body>
+</html>
