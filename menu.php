4e83c5015a8dba0abd6a4742c26d35da2f67dbde
ArturD00 <arturdembonczyk00@gmail.com>
2023-04-04 11:05:50 +0200

    mechanizm pojawiania się oraz znikania okienka koszyka

--- menu.php
+++ menu.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-phpinfo();
 <html>
 <head>
     
@@ -9,10 +8,14 @@ phpinfo();
 </head>
 <body>
    <div id="container">
-
+    <div id="Basket">
+        <table id="BasketTable">
+        </table>
+    </div>
 
     <header >
-        <!-- zawartość nagłówka strony -->
+    <button onclick="SetVisibility()" id="BasketButton">
+    </button>
     </header>
 
     <nav>
@@ -41,58 +44,85 @@ phpinfo();
 
                  while($wiersz = $wynik->fetch_assoc())
                 {
-                    $thisid = $wiersz["Id"];
-                    $ingredientString = $wiersz["Ingredients"];
-                    $fullIngredients ="";
-                    $tempHelp="";
+                        $thisid = $wiersz["Id"];
+                        $ingredientString = $wiersz["Ingredients"];
+                        $fullIngredients ="";
+                        $tempHelp="";
 
 
 
 
-                    for($number=0;$number<strlen($ingredientString);$number++)
-                    {
-                        if($ingredientString[$number]==","  || $number+1==strlen($ingredientString))
+                        for($number=0;$number<strlen($ingredientString);$number++)
                         {
-                            $thisIngrediant = $conn->query("select * from ingredients where id=$tempHelp");
-                         
-                                $mytemprow = $thisIngrediant->fetch_row();
-                              
-                                if($number+1==strlen($ingredientString))
-                                {
-                                    $fullIngredients .=$mytemprow[1];
-                                    break;
-                                }
-                                $fullIngredients .=$mytemprow[1] . ", ";
-                            $tempHelp="";
+                            if($ingredientString[$number]==","  || $number+1==strlen($ingredientString))
+                            {
+                                $thisIngrediant = $conn->query("select * from ingredients where id=$tempHelp");
+                            
+                                    $mytemprow = $thisIngrediant->fetch_row();
+                                
+                                    if($number+1==strlen($ingredientString))
+                                    {
+                                        $fullIngredients .=$mytemprow[1];
+                                        break;
+                                    }
+                                    $fullIngredients .=$mytemprow[1] . ", ";
+                                $tempHelp="";
+                            }
+                            else
+                            {
+                                //$tempHelp+=$ingredientString[$number];
+                                $tempHelp .= $ingredientString[$number];
+                            }
                         }
-                        else
-                        {
-                            //$tempHelp+=$ingredientString[$number];
-                            $tempHelp .= $ingredientString[$number];
-                        }
-                    }
-                  
+                    
 
-                        $thisid = "menu".$thisid;
-                    echo <<<HTML
-              <div class="menu_cell" >
-                   <button id= $thisid>
-                    <p class="menuName"></p>
-                   <p class="menuIngrediants">{$fullIngredients}</p>
-                </button>
-              </div>
-            HTML;
+                            $thisid = "menu".$thisid;
+                        echo <<<HTML
+                    <div class="menu_cell" >
+                        <button id= $thisid onclick="AddToBasket(id)">
+                            <p class="menuName"></p>
+                        <p class="menuIngrediants">{$fullIngredients}</p>
+                        </button>
+                    </div>
+                    HTML;
+//AddToBasket($wiersz["Id"] , $fullIngredients,$wiersz["Cost_S"] , $wiersz["Cost_L"] )
 
+
+                    //AddToBasket($pizzaID,$pizzaIng,$pizzaCostS,$pizzaCostL)
 
                  
                  }
+                
+              
            }
 
-        
-
-
     ?>   
-    </main>
+
+                <script>
+                 function SetVisibility()
+                 {
+                    var x = document.getElementById("Basket");
+                if(x.style.display=="none")
+                    x.style.display="block";
+                else
+                x.style.display="none"
+
+                 }
+
+
+
+                 function AddToBasket($pizzaID)
+                 {
+                    var x = document.getElementById("BasketTable");
+                    var newRow = x.insertRow();
+                    var newCell1 = newRow.insertCell();
+                    var newText = document.createTextNode($pizzaID);
+                    newCell1.appendChild(newText);
+                 }
+
+
+                </script>
+     </main>
 
     <footer>
         <!-- zawartość stopki strony -->
