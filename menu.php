8574691cf5f91f457bd568809fa07ff881be8ab8
Mateusz Kuźnia <matix666@outlook.pl>
2023-04-18 13:12:50 +0200

    update file
    update 13:12 18.04.2023

--- menu.php
+++ menu.php
@@ -1,4 +1,7 @@
 <?php
+
+//12:35 18.04.2023
+
 // ustawienia bazy danych
 $servername = "localhost";
 $username = "username";
@@ -18,10 +21,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    
+    $confirm_password = $_POST["confirm_password"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+
     // walidacja formularza
-    if (empty($username) || empty($email) || empty($password)) {
+    if (empty($username) || empty($email) || empty($password) || empty($confirm_password) || empty($first_name) || empty($last_name)) {
         echo "Please fill in all required fields.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format.";
+    } elseif ($password !== $confirm_password) {
+        echo "Passwords do not match.";
     } else {
         // sprawdzenie, czy użytkownik o takim adresie email już istnieje
         $sql = "SELECT * FROM users WHERE email='$email'";
@@ -34,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
             
             // dodanie nowego użytkownika do bazy danych
-            $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+            $sql = "INSERT INTO users (username, email, password, first_name, last_name) VALUES ('$username', '$email', '$hashed_password', '$first_name', '$last_name')";
             
             if (mysqli_query($conn, $sql)) {
                 echo "New user created successfully.";
@@ -44,7 +54,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 }
-
 // zamykanie połączenia z bazą danych
 mysqli_close($conn);
 ?>
